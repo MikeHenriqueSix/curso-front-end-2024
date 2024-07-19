f58128024c27ec3de1c3f834000107e47bf5fd9b
MikeHenriqueSix <murillohslopes@gmail.com>
2024-07-18 21:52:12 -0300

    18/07/2024

---
+++ 6-JavaScript/index.js
@@ -0,0 +1,48 @@
+// ctrl + / = comentar   ctrl + L = limpar console
+
+// alert("Hello World!");
+// console.log("Ola Mundo!");
+// prompt("Escreva seu Nome: \n")  typeof
+
+// var sobrenome = prompt ("Escreva seu Sobrenome: \n")
+// var myName = "Mike " + sobrenome
+// var myAge = 20;
+// .length - usado para contar caracters
+// .slice(0, 10); - usado para pegar um caracter
+// .toLowerCase(); - Usado para deixar em letra minuscula
+// .toUpperCase(); - Usado para deixar em letra maiuscula
+// var texto = "Hello World!";
+
+// var nome = prompt("Nome: ")
+// nome = nome.slice(0, 1).toUpperCase() + nome.slice(1, nome.length).toLowerCase();
+
+// console.log(nome);
+
+
+
+
+
+// Desafio_8
+
+// var seuNome = prompt("Digite seu nome:  Ex. Fabio")
+// var suaIdade = prompt("Digite sua idade: Ex. 100")
+
+
+// var mensagem = "" + seuNome + "" + suaIdade + ""
+// alert(mensagem)
+
+
+// alert("Seu nome é: \n" + seuNome + "\ne você tem: \n" + suaIdade + " Anos")
+
+
+
+
+
+// Desafio_9
+
+// var tweet = prompt("Escreva seu Tweet: \n");
+// tweet = tweet.slice(0, 140);
+// alert(tweet + "\n\n O Texto Maximo é de 140 !");
+
+
+// console.log(tweet.length);
